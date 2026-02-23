76030d9a8b31d5a51243e4d20679473073787d9d
DavisThuillier <dsthuillier@gmail.com>
2026-02-22 22:32:37 -0800

    Adding array jobs and final compile job for queuing example

--- queuing/pipeline.sh
+++ queuing/pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Submit the array job and capture its main Job ID
-FIRST_JOB_ID=$(sbatch array_job.sub | cut -d' ' -f4)
+FIRST_JOB_ID=$(sbatch --parsable array_job.sub)
 echo "Submitted array job with ID: $FIRST_JOB_ID"
 
 # Submit the dependent job
-SECOND_JOB_ID=$(sbatch --dependency=afterany:$FIRST_JOB_ID after.sub| cut -d' ' -f4)
+SECOND_JOB_ID=$(sbatch --dependency=afterok:$FIRST_JOB_ID anthology.sub)
 echo "Submitted dependent job $SECOND_JOB_ID to run after array job $FIRST_JOB_ID"
